19a78d01bd67033ee3a82bf2ed38387f8821609b
Shameed4 <94253513+Shameed4@users.noreply.github.com>
2022-11-11 23:47:52 -0500

    Changed stuff

--- UI/script.js
+++ UI/script.js
@@ -1,98 +1,32 @@
-var text = document.getElementById("output");
-var allKeys = document.querySelectorAll("#tapButtons > *, .wasdLayout button");
-
+var buttons = document.querySelectorAll(".tap");
 var joysticks = document.getElementsByClassName("joystick");
-var keys = document.getElementsByClassName("wasdLayout");
-
 var input = [0,0,0,0,0,0];
-console.log(input);
-switchKeys(false);
 
-for (let i = 0; i < allKeys.length; i++) {
-    let key = allKeys[i];
-    key.addEventListener("mousedown", function(event) {
-        enter(event, key);
-    });
-    key.addEventListener("mouseup", function(event) {
-        exit(event, key);
-    });
-    key.addEventListener("touchstart", function(event) {
-        enter(event, key);
-    });
-    key.addEventListener("touchend", function(event) {
-        exit(event, key);
-    });
-    console.log(key.innerText);
-}
-
-document.addEventListener('keydown', function(event) {
-    enter(event, null);
-});
-document.addEventListener('keyup', function(event) {
-    exit(event, null);
-});
-
-function interpretLetter(e) {
-    return e.key.toLowerCase();
-}
-
-function enter(e, key) {
-    if (key == null)
-        key = document.getElementById(interpretLetter(e));
-    if (key == null)
-        return;      
-    text.innerText = e.key + "\n" + text.innerText;
-    if (key.hasAttribute("lock"))
-        return;
-    key.toggleAttribute("lock");
-    key.toggleAttribute("on");
-    
-    if (key.hasAttribute("out")) {
-        if (key.hasAttribute("on"))
-            input[key.getAttribute("out")] += parseInt(key.getAttribute("val"));
-        else
-            input[key.getAttribute("out")] -= parseInt(key.getAttribute("val"));
-    }
-    console.log(input);
-}
-
-function exit(e, key) {
-    if (key == null)
-        key = document.getElementById(interpretLetter(e));
-    if (key == null)
-        return; 
-    text.innerText = e.key + "\n" + text.innerText;
-    key.toggleAttribute("lock");
-    
-    if (key.getAttribute("type") == "press") {
-        key.toggleAttribute("on");  
-        if (key.hasAttribute("out")) {
-            if (key.hasAttribute("on"))
-                input[key.getAttribute("out")] += parseInt(key.getAttribute("val"));
-            else
-                input[key.getAttribute("out")] -= parseInt(key.getAttribute("val"));
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mousedown", function(e) {
+        this.toggleAttribute("on");
+        if (this.hasAttribute("on")) {
+            input[parseInt(this.getAttribute("out"))] = 1;
         }
-    }
-    console.log(input);
+        else {
+            input[parseInt(this.getAttribute("out"))] = 0;
+        }
+        inputToString();
+    });
 }
 
 function joystickOut(val, o) {
-    input[o] = val / 100;
+    input[o] = val;
     console.log(input);
+    let str = "";
+    for (let i = 0; i < input.length; i++) {
+        str += str + " ";
+    }
+    inputToString();
 }
 
-function switchKeys(isKeyboard) {
-    if (isKeyboard)
-    {
-        for (let i = 0; i < joysticks.length; i++) {
-            joysticks[i].style.display = "none";
-            keys[i].style.display = "block";
-        }
-    }
-    else {
-        for (let i = 0; i < joysticks.length; i++) {
-            joysticks[i].style.display = "block";
-            keys[i].style.display = "none";
-        }
-    }
+function inputToString() {
+    let o = input.join(" ");
+    console.log(o);
+    // sendToBot(o);
 }
